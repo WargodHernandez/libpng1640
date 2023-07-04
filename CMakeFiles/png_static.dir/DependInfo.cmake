97178fdff1e7a9bcda76375d62d410d77a32f7e2
Chris Brown <WargodHernandez@users.noreply.github.com>
2023-07-04 14:54:27 -0500

    Copied from SF

---
+++ CMakeFiles/png_static.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/brown/AppData/Local/Temp/pip-install-alldnudv/pillow-simd_97ec4622df614db39fbf454605735010/winbuild/build/lpng1640/png.c" "CMakeFiles/png_static.dir/png.c.obj" "msvc" "CMakeFiles/png_static.dir/png.c.obj.d"
+  "C:/Users/brown/AppData/Local/Temp/pip-install-alldnudv/pillow-simd_97ec4622df614db39fbf454605735010/winbuild/build/lpng1640/pngerror.c" "CMakeFiles/png_static.dir/pngerror.c.obj" "msvc" "CMakeFiles/png_static.dir/pngerror.c.obj.d"
+  "C:/Users/brown/AppData/Local/Temp/pip-install-alldnudv/pillow-simd_97ec4622df614db39fbf454605735010/winbuild/build/lpng1640/pngget.c" "CMakeFiles/png_static.dir/pngget.c.obj" "msvc" "CMakeFiles/png_static.dir/pngget.c.obj.d"
+  "C:/Users/brown/AppData/Local/Temp/pip-install-alldnudv/pillow-simd_97ec4622df614db39fbf454605735010/winbuild/build/lpng1640/pngmem.c" "CMakeFiles/png_static.dir/pngmem.c.obj" "msvc" "CMakeFiles/png_static.dir/pngmem.c.obj.d"
+  "C:/Users/brown/AppData/Local/Temp/pip-install-alldnudv/pillow-simd_97ec4622df614db39fbf454605735010/winbuild/build/lpng1640/pngpread.c" "CMakeFiles/png_static.dir/pngpread.c.obj" "msvc" "CMakeFiles/png_static.dir/pngpread.c.obj.d"
+  "C:/Users/brown/AppData/Local/Temp/pip-install-alldnudv/pillow-simd_97ec4622df614db39fbf454605735010/winbuild/build/lpng1640/pngread.c" "CMakeFiles/png_static.dir/pngread.c.obj" "msvc" "CMakeFiles/png_static.dir/pngread.c.obj.d"
+  "C:/Users/brown/AppData/Local/Temp/pip-install-alldnudv/pillow-simd_97ec4622df614db39fbf454605735010/winbuild/build/lpng1640/pngrio.c" "CMakeFiles/png_static.dir/pngrio.c.obj" "msvc" "CMakeFiles/png_static.dir/pngrio.c.obj.d"
+  "C:/Users/brown/AppData/Local/Temp/pip-install-alldnudv/pillow-simd_97ec4622df614db39fbf454605735010/winbuild/build/lpng1640/pngrtran.c" "CMakeFiles/png_static.dir/pngrtran.c.obj" "msvc" "CMakeFiles/png_static.dir/pngrtran.c.obj.d"
+  "C:/Users/brown/AppData/Local/Temp/pip-install-alldnudv/pillow-simd_97ec4622df614db39fbf454605735010/winbuild/build/lpng1640/pngrutil.c" "CMakeFiles/png_static.dir/pngrutil.c.obj" "msvc" "CMakeFiles/png_static.dir/pngrutil.c.obj.d"
+  "C:/Users/brown/AppData/Local/Temp/pip-install-alldnudv/pillow-simd_97ec4622df614db39fbf454605735010/winbuild/build/lpng1640/pngset.c" "CMakeFiles/png_static.dir/pngset.c.obj" "msvc" "CMakeFiles/png_static.dir/pngset.c.obj.d"
+  "C:/Users/brown/AppData/Local/Temp/pip-install-alldnudv/pillow-simd_97ec4622df614db39fbf454605735010/winbuild/build/lpng1640/pngtrans.c" "CMakeFiles/png_static.dir/pngtrans.c.obj" "msvc" "CMakeFiles/png_static.dir/pngtrans.c.obj.d"
+  "C:/Users/brown/AppData/Local/Temp/pip-install-alldnudv/pillow-simd_97ec4622df614db39fbf454605735010/winbuild/build/lpng1640/pngwio.c" "CMakeFiles/png_static.dir/pngwio.c.obj" "msvc" "CMakeFiles/png_static.dir/pngwio.c.obj.d"
+  "C:/Users/brown/AppData/Local/Temp/pip-install-alldnudv/pillow-simd_97ec4622df614db39fbf454605735010/winbuild/build/lpng1640/pngwrite.c" "CMakeFiles/png_static.dir/pngwrite.c.obj" "msvc" "CMakeFiles/png_static.dir/pngwrite.c.obj.d"
+  "C:/Users/brown/AppData/Local/Temp/pip-install-alldnudv/pillow-simd_97ec4622df614db39fbf454605735010/winbuild/build/lpng1640/pngwtran.c" "CMakeFiles/png_static.dir/pngwtran.c.obj" "msvc" "CMakeFiles/png_static.dir/pngwtran.c.obj.d"
+  "C:/Users/brown/AppData/Local/Temp/pip-install-alldnudv/pillow-simd_97ec4622df614db39fbf454605735010/winbuild/build/lpng1640/pngwutil.c" "CMakeFiles/png_static.dir/pngwutil.c.obj" "msvc" "CMakeFiles/png_static.dir/pngwutil.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
